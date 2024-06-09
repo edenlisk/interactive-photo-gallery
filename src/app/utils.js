d6a2675d0961cc88415f7aa44824e09a13be8828
edenlisk <nkumbuyenock@gmail.com>
2024-06-09 16:55:14 +0200

    Refactored to separate data from actual functionality logic

---
+++ src/app/utils.js
@@ -0,0 +1,26 @@
+const fennecFoxImg = require("../assets/images/fennecFox.png");
+const humpbackWhaleImg = require("../assets/images/humpbackWhale.png");
+const commonBrownBaboonImg = require("../assets/images/commonBrownBaboon.png");
+const spottedDeerImg = require("../assets/images/spottedDeer.png");
+exports.galleryData = [
+    {
+        imageSource: fennecFoxImg,
+        title: ["FENNEC", "FOX"],
+        location: "India",
+    },
+    {
+        imageSource: humpbackWhaleImg,
+        title: ["HUMPBACK", "WHALE"],
+        location: "South Africa",
+    },
+    {
+        imageSource: commonBrownBaboonImg,
+        title: ["COMMON BROWN", "BABOON"],
+        location: "South Africa",
+    },
+    {
+        imageSource: spottedDeerImg,
+        title: ["SPOTTED", "DEER"],
+        location: "Amazon",
+    },
+]
--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
-const fennecFoxImg = require('./assets/images/fennecFox.png');
-const humpbackWhaleImg = require('./assets/images/humpbackWhale.png');
-const commonBrownBaboonImg = require('./assets/images/commonBrownBaboon.png');
-const spottedDeerImg = require('./assets/images/spottedDeer.png');
 require('./style.css');
-
-const galleryData = [
-    {
-        imageSource: fennecFoxImg,
-        title: ["FENNEC", "FOX"],
-        location: "India",
-    },
-    {
-        imageSource: humpbackWhaleImg,
-        title: ["HUMPBACK", "WHALE"],
-        location: "South Africa",
-    },
-    {
-        imageSource: commonBrownBaboonImg,
-        title: ["COMMON BROWN", "BABOON"],
-        location: "South Africa",
-    },
-    {
-        imageSource: spottedDeerImg,
-        title: ["SPOTTED", "DEER"],
-        location: "Amazon",
-    },
-]
+const { galleryData } = require('./app/utils');
 
 const galleryContainer = document.querySelector('.gallery-container ');
 galleryData.forEach((data) => {
