ef43e1c38696f864a0132e96f099eab3cde14e6e
edenlisk <nkumbuyenock@gmail.com>
2024-06-09 15:33:03 +0200

    Added dependencies and re-configure webpack to bundle assets

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,33 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.(js|jsx)$/, // Rule for JavaScript and JSX files
+                exclude: /node_modules/, // Exclude node_modules folder
+                use: {
+                    loader: 'babel-loader', // Use Babel loader
+                },
+            },
+            {
+                test: /\.css$/, // Rule for CSS files
                 use: [
-                    "style-loader",
-                    "css-loader"
-                ]
+                    'style-loader', // Injects CSS into the DOM
+                    'css-loader', // Parses CSS
+                ],
+            },
+            {
+                test: /\.(jpg|png)$/,
+                type: 'asset/resource',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 900 * 1024 // 900KB
+                    }
+                },
+            },
+            {
+                test: /\.txt/,
+                type: 'asset/source'
             }
-        ]
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
